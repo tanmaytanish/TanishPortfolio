e5a41c7cd1e6dbc489734a731c5492090a709bc4
Tanish Tanmay Sahoo <121039428+tanmaytanish@users.noreply.github.com>
2026-03-12 19:32:26 +0530

    feat: add hero section with animated text and a responsive 3D computer model.

--- src/components/canvas/computers.tsx
+++ src/components/canvas/computers.tsx
@@ -46,7 +46,7 @@ const Computers = ({ isMobile }: ComputersProps) => {
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.65}
-        position={isMobile ? [0, -3, -2.2] : [0, -2.5, -1]}
+        position={isMobile ? [0, -1.5, -2.2] : [0, -2.5, -1]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
