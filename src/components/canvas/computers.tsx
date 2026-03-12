b5c1e4f71ac7ef8ea05d5e3a65ad988da80b9b6b
Tanish Tanmay Sahoo <121039428+tanmaytanish@users.noreply.github.com>
2026-03-12 19:37:02 +0530

    feat: Add 3D desktop computer model component with responsive rendering, lighting, and material adjustments.

--- src/components/canvas/computers.tsx
+++ src/components/canvas/computers.tsx
@@ -84,6 +84,7 @@ const ComputersCanvas = () => {
         {/* Controls */}
         <OrbitControls
           enableZoom={false}
+          enablePan={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
